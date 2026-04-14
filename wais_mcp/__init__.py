37c1625f714a5b292d349f8e6a5d357c89661fd9
Deck <deck@deeger.io>
2026-04-14 13:59:34 +0200

    Bump to v0.2.2

--- wais_mcp/__init__.py
+++ wais_mcp/__init__.py
@@ -1,6 +1,6 @@
 """Universal WAIS MCP server and SDK for any WAIS-compatible site."""
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 from .client import WAISClient
 from .manifest import WAISManifest
